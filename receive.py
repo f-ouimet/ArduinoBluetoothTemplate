c8dcd46f75677adc781b180aa28e88b2828309bd
f_ouimet <felix.ouimet@gmail.com>
2025-03-18 14:15:57 -0400

    Update receive.py

--- receive.py
+++ receive.py
@@ -2,12 +2,16 @@
 import bluetooth
 #Template MAC address, use your own
 address = 'FF:00:FF:AA:FF:FF'
+#port can be changed according to needs
 port = 1
+#data size
 size = 1024
 #Bluetooth socket
 s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 socket.connect((address, port))
-buffer = ""
+#string to print
+buff = ""
+#infinite loop
 while True:
     data = socket.recv(1024)
     buff = data.decode('ascii')
